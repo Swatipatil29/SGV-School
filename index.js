b82d986a9581ae30d4d09688ea83521bf38e885f
swati.b2 <swati.b@simpragma.com>
2025-04-13 16:32:49 +0530

    fix: bind to process.env.PORT for Railway

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000;
 const mongodb = require("./configuration/config")
 const {checkSchema} = require('express-validator')
 const cors = require("cors")
