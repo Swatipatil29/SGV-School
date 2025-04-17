783dcb62ab2e6b5878ec03477d222bb0541105d9
swati.b2 <swati.b@simpragma.com>
2025-04-17 07:54:35 +0530

    changed mongodb configuration

--- configuration/config.js
+++ configuration/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const configureDb = async () =>  {
- const db = await mongoose.connect('mongodb://127.0.0.1:27017/school-app')
+ const db = await mongoose.connect('mongodb+srv://avinashkempi:Rcbcsk@mongodb123@schoolapp.uhmjiaw.mongodb.net/?retryWrites=true&w=majority&appName=SchoolApp')
 
  try{
     console.log('Connected to db')
